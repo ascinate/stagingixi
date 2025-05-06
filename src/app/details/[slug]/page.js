24a84c687c48ad20ce3f2fe90c6bfb779945a251
samrat <samrat.subratadas@gmail.com>
2025-05-06 18:35:05 +0530

    details changes

--- src/app/details/[slug]/page.js
+++ src/app/details/[slug]/page.js
@@ -235,9 +235,9 @@ const id = slug?.split('_').pop();
                        <div className="col-lg-6 col-xl-5 buttons-group01 d-grid justify-content-end">
                               <div className="right-details-lis01">
                                   <div className="groups-list-btn">
-                                      <div className="comon-groups-div01 d-flex align-items-center justify-content-between">
-                                           <h5 className="m-0"> Size : </h5>
-                                           <div className="input-divs d-flex align-items-center">
+                                      <div className="comon-groups-div01 align-items-center justify-content-between">
+                                           <h5 className="m-0 w-100"> Pick Your Dimensions: </h5>
+                                           <div className="input-divs w-100 mt-3 d-flex align-items-center">
                                             {!showCustom ? (
                                                 <select
                                                   id="size"
@@ -291,12 +291,18 @@ const id = slug?.split('_').pop();
 
                                            </div>
                                       </div>
-                                      <div className="comon-groups-div01 d-flex align-items-center justify-content-between">
-                                           <h5 className="m-0"> Download As: </h5>
-                                           <div className="input-divs d-flex justify-content-between align-items-center">
-                                               <button type="button" onClick={handleDownloadSVG} className="btn btn-comons01 crm-btn01"> SVG </button>
-                                               <button type="button" onClick={() => svgToCanvasDownload("png")} className="btn btn-comons01 crm-btn01"> PNG </button>
-                                               <button type="button" onClick={() => svgToCanvasDownload("webp")} className="btn btn-comons01 crm-btn01"> WEBP </button>
+                                      <div className="comon-groups-div01 mb-3 align-items-center justify-content-between">
+                                           <h5 className="m-0 w-100"> Download This File As.. </h5>
+                                           <div className="input-divs mt-3 w-100 d-flex justify-content-between align-items-center">
+                                               <button type="button" onClick={handleDownloadSVG} className="btn svg-bn btn-comons01 crm-btn01">
+                                               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18" fill="rgba(255,255,255,1)"><path d="M2.80577 5.20006L7.00505 7.99958L11.1913 2.13881C11.5123 1.6894 12.1369 1.58531 12.5863 1.90631C12.6761 1.97045 12.7546 2.04901 12.8188 2.13881L17.0051 7.99958L21.2043 5.20006C21.6639 4.89371 22.2847 5.01788 22.5911 5.47741C22.7228 5.67503 22.7799 5.91308 22.7522 6.14895L21.109 20.1164C21.0497 20.62 20.6229 20.9996 20.1158 20.9996H3.8943C3.38722 20.9996 2.9604 20.62 2.90115 20.1164L1.25792 6.14895C1.19339 5.60045 1.58573 5.10349 2.13423 5.03896C2.37011 5.01121 2.60816 5.06832 2.80577 5.20006ZM12.0051 14.9996C13.1096 14.9996 14.0051 14.1042 14.0051 12.9996C14.0051 11.895 13.1096 10.9996 12.0051 10.9996C10.9005 10.9996 10.0051 11.895 10.0051 12.9996C10.0051 14.1042 10.9005 14.9996 12.0051 14.9996Z"></path></svg>
+                                                SVG </button>
+                                               <button type="button" onClick={() => svgToCanvasDownload("png")} className="btn png-bn btn-comons01 crm-btn01">
+                                               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16" fill="rgba(0,0,0,1)"><path d="M2.9918 21C2.44405 21 2 20.5551 2 20.0066V3.9934C2 3.44476 2.45531 3 2.9918 3H21.0082C21.556 3 22 3.44495 22 3.9934V20.0066C22 20.5552 21.5447 21 21.0082 21H2.9918ZM20 15V5H4V19L14 9L20 15ZM20 17.8284L14 11.8284L6.82843 19H20V17.8284ZM8 11C6.89543 11 6 10.1046 6 9C6 7.89543 6.89543 7 8 7C9.10457 7 10 7.89543 10 9C10 10.1046 9.10457 11 8 11Z"></path></svg>
+                                                PNG </button>
+                                               <button type="button" onClick={() => svgToCanvasDownload("webp")} className="btn png-bn btn-comons01 crm-btn01">
+                                               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16" fill="rgba(0,0,0,1)"><path d="M2.9918 21C2.44405 21 2 20.5551 2 20.0066V3.9934C2 3.44476 2.45531 3 2.9918 3H21.0082C21.556 3 22 3.44495 22 3.9934V20.0066C22 20.5552 21.5447 21 21.0082 21H2.9918ZM20 15V5H4V19L14 9L20 15ZM20 17.8284L14 11.8284L6.82843 19H20V17.8284ZM8 11C6.89543 11 6 10.1046 6 9C6 7.89543 6.89543 7 8 7C9.10457 7 10 7.89543 10 9C10 10.1046 9.10457 11 8 11Z"></path></svg>
+                                                WEBP </button>
                                            </div>
                                       </div>
 
