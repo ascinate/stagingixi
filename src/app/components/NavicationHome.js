7a22ffedf42ba2922a34a083f04f2972929b38ab
ski12345491 <abu@ascinatetech.com>
2025-05-12 15:20:59 +0530

    logo fix

--- src/app/components/NavicationHome.js
+++ src/app/components/NavicationHome.js
@@ -17,8 +17,8 @@ function NavicationHome(){
                 <Link className="navbar-brand" href='/'>
                    <Image loading="lazy" src="/icons_guru_logo.svg"
                         alt="iconsguru"
-                        width={147}
-                        height={22} />
+                        width={180}
+                        height={44} />
                 </Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                    <span className="navbar-toggler-icon"></span>
