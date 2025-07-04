968b6cd1de153765cfce71351621cbcfdf36c17a
ski12345491 <abu@ascinatetech.com>
2025-07-04 16:02:47 +0530

    login auth check

--- src/app/components/NavicationHome.js
+++ src/app/components/NavicationHome.js
@@ -1,6 +1,7 @@
+"use client"
 import Link from "next/link";
 import Image from "next/image";
-import Form from 'next/form';
+
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 
