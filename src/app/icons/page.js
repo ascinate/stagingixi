c26619d495c668723fe5644b581b1b0d6c7014e5
ski12345491 <abu@ascinatetech.com>
2025-08-07 17:47:45 +0530

    thumnails first

--- src/app/icons/page.js
+++ src/app/icons/page.js
@@ -32,6 +32,7 @@ export default function Icons() {
     '/gardening.svg',
     '/halloween.svg',
     '/health.svg',
+    '/heart.svg',
     '/hieroglyphs.svg',
     '/home.svg',
     '/interface.svg',
